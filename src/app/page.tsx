cb9c69870b44cedd0b59e4224f54bfc63262c5a7
Marcos Segantine <msegantine8@gmail.com>
2023-07-11 22:32:35 -0300

    Adding initial content from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
+import home from "../styles/home.module.css"
+import Image from "next/image"
+
+import banner from "../assets/img/banner_home.png"
+
+import { Button } from "@/components/Button"
+
 const Home = () => {
   return (
-    <main></main>
+    <main className={home.home}>
+      <div className={home.home__init}>
+        <Image
+          className={home.home__init__banner}
+          src={banner}
+          alt=""
+        />
+
+        <div className={home.home__init__content}>
+          <h4 className={home.home__init__content__subTitle}>Juntos podemos</h4>
+          <h1 className={home.home__init__content__title}>Criar um mundo melhor para os jovens</h1>
+          <Button 
+            text="Seja um voluntário"
+            gradient={true}
+          />
+        </div>
+
+      </div>
+    </main>
   )
 }
 
