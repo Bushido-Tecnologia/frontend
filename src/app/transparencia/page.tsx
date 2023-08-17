d0916c19faea5b9d581ec58768df94c05cbd9855
Marcos Segantine <msegantine8@gmail.com>
2023-08-17 12:54:51 -0300

    Adding pdf files to transparência page

--- src/app/transparencia/page.tsx
+++ src/app/transparencia/page.tsx
@@ -7,6 +7,7 @@ import preview1 from "../../assets/img/transparency/preview1.png"
 import preview2 from "../../assets/img/transparency/preview2.png"
 import preview3 from "../../assets/img/transparency/preview3.png"
 import preview4 from "../../assets/img/transparency/preview4.png"
+import preview5 from "../../assets/img/transparency/preview5.png"
 
 const Page = () => {
     return (
@@ -18,24 +19,35 @@ const Page = () => {
                     title="Estatuto social"
                     description="Estatuto social é o nível de valor social que uma pessoa é considerada a possuir. Mais especificamente, refere-se ao nível relativo de respeito, honra, competência e influência concedida a pessoas, grupos e organizações em uma sociedade."
                     imagePreview={preview1}
+                    reportName="ESTATUTO ATUALIZADO EM 2022 REFORMA DO ESTATUTO0001-1.pdf"
                 />
 
-                <Report 
+                <Report
                     title="Balanço, balancete e DRE 2022/2023"
                     description="Balanço patrimonial e DRE são relatórios contábeis que as empresas devem elaborar. Trata-se de um dever previsto em lei,   que exige a entrega dos relatórios por toda pessoa jurídica registrada no Brasil ao final de cada exercício social"
                     imagePreview={preview2}
+                    reportName="BALANCO-BALANCETE-E-DRE.pdf"
                 />
 
-                <Report 
+                <Report
                     title="Relatório anual"
                     description="Relatório Anual é uma publicação que apresenta as informações institucionais, os resultados e metas e os indicadores qualitativos e quantitativos referentes às atividades das empresas durante o período anual do exercício anterior."
                     imagePreview={preview3}
+                    reportName="RELATORIO-ANUAL.pdf"
                 />
 
-                <Report 
+                <Report
                     title="Relatório Conselho Fiscal 2023"
                     description="O relatório do conselho fiscal é aquele que inclui todas as questões relativas à apreciação das contas do síndico. Também chamado de parecer, ele traz eventuais ressalvas à aprovação ou à rejeição das contas apresentadas pelo síndico naquele exercício."
                     imagePreview={preview4}
+                    reportName="PARECR-DO-CONSELHO-FISCAL-2022-A-2023.pdf"
+                />
+
+                <Report
+                    title="Ato de nomeação da diretoria"
+                    description="Ato que marca o início da investidura no cargo público, efetivo ou comissionado, que se completa com a posse e o exercício - realizados simultaneamente."
+                    imagePreview={preview5}
+                    reportName="ATA DE ELEIÇÃO 2023 A 2027.pdf"
                 />
             </div>
         </main>
--- src/components/Report/index.tsx
+++ src/components/Report/index.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import styles from "./style.module.css"
 
 import { StaticImageData } from "next/image";
@@ -6,10 +8,15 @@ import Image from "next/image";
 interface Props {
     title: string;
     description: string;
-    imagePreview: StaticImageData
+    imagePreview: StaticImageData;
+    reportName: string
 }
 
-export const Report: React.FC<Props> = ({ description, imagePreview, title }) => {
+export const Report: React.FC<Props> = ({ description, imagePreview, title, reportName }) => {
+    const openPdfInNewTab = () => {
+        window.open(`/reports/${reportName}`, '_blank');
+      };
+
     return (
         <div className={styles.report}>
             <div className={styles.report__content}>
@@ -17,7 +24,7 @@ export const Report: React.FC<Props> = ({ description, imagePreview, title }) =>
                 <p className={styles.report__content__description}>{description}</p>
             </div>
 
-            <div className={styles.report__image}>
+            <div className={styles.report__image} onClick={openPdfInNewTab}>
                 <Image src={imagePreview} alt="" />
             </div>
         </div>
